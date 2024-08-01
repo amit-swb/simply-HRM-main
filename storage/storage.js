9509e21991327de59be14e2b51077a5736c519a5
amit-swb <amit@silverwebbuzz.com>
2024-08-01 10:55:47 +0530

    chanage in cloudinary storage name

--- storage/storage.js
+++ storage/storage.js
@@ -12,7 +12,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: "Hrm",
+    folder: "hrm",
     allowedFormats: ["jpeg", "png", "jpg"],
   },
 });
